eaa86516e0e3179e11f6af24e8f8c9f7b9981b8c
Maud Mulder <45165871+MaudMulder@users.noreply.github.com>
2018-12-17 10:55:39 +0100

    Bnummer script

---
+++ BingoA6/Assets/Script/Bnummer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bnummer : MonoBehaviour {
+
+	public string vak_aanduiding;
+	private int vak_nummer;
+ 
+	// Use this for initialization
+	void Start () {
+		vak_nummer = Random.Range(1,14);
+		vak_aanduiding = "B" + vak_nummer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
