7be8b0c3d12db335829c68291d1a952bef0b5b05
Maud Mulder <45165871+MaudMulder@users.noreply.github.com>
2018-12-20 10:48:21 +0100

    button

---
+++ BingoA6/Assets/Script/ButtonRad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonRad : MonoBehaviour {
+
+private void button1_Click(object sender, EventArgs e) 
+	 {
+            textBox1.Text = "a";
+        }
+
+}
