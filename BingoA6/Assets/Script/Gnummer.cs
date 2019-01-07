88e171cdb66aa47965451eb83fc06a53d9ec4b4c
Maud Mulder <45165871+MaudMulder@users.noreply.github.com>
2019-01-07 11:06:00 +0100

    Random nummer kaart

--- BingoA6/Assets/Script/Gnummer.cs
+++ BingoA6/Assets/Script/Gnummer.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Gnummer : MonoBehaviour {
 
+	//public string vak_aanduiding;
+	private int vak_nummer;
+	private TextMesh vak_aanduiding;
+ 
 	// Use this for initialization
 	void Start () {
-		
+		vak_nummer = Random.Range(44,57);
+		// vak_aanduiding = "B" + vak_nummer;
+		vak_aanduiding = gameObject.GetComponentInChildren(typeof(TextMesh)) as TextMesh;
+		vak_aanduiding.text = "" + vak_nummer;
 	}
 	
 	// Update is called once per frame
@@ -14,3 +21,5 @@ public class Gnummer : MonoBehaviour {
 		
 	}
 }
+
+
